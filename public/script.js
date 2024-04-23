067bc5dd41749cf12f5706c774de08c920e7a5d5
AndiiCodes <aandy.evan@gmail.com>
2024-04-24 00:36:37 +1000

    updated the code for lenneth

--- public/script.js
+++ public/script.js
@@ -2,38 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   fetchAndDisplayCourses();
 });
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   // Show the loader
-//   document.getElementById("loader").style.display = "block";
-
-//   try {
-//     // Fetch and display courses
-//     await fetchAndDisplayCourses();
-
-//     // Hide the loader
-//     document.getElementById("loader").style.display = "none";
-//     // Show the container
-//     document.getElementById("container").style.display = "block";
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// });
-
 async function fetchAndDisplayCourses() {
   try {
-    // Updated to use relative URL
     const response = await fetch("https://j3ba.onrender.com/api/courses");
     if (!response.ok) {
       throw new Error("Failed to fetch courses");
     }
     const courses = await response.json();
     const container = document.getElementById("container");
-    container.innerHTML = ""; // Clear previous content
-
+    container.innerHTML = "";
     courses.forEach((course) => {
       if (
-        course.name !== "Succeed VE 2024" &&
-        course.name !== "Dip of Information Technology (2405)"
+        course.name !== "Dip of Information Technology (2345)" &&
+        course.name !== "How2RMIT"
       ) {
         const card = document.createElement("div");
         card.classList.add("card");
@@ -54,7 +35,6 @@ async function fetchAndDisplayCourses() {
 
 async function fetchAndDisplayAssessments(courseId) {
   try {
-    // Updated to use relative URL
     const response = await fetch(
       `https://j3ba.onrender.com/api/courses/${courseId}/assignments`
     );
@@ -63,7 +43,7 @@ async function fetchAndDisplayAssessments(courseId) {
     }
     const assessments = await response.json();
     const container = document.getElementById("assessment-container");
-    container.innerHTML = ""; // Clear previous content
+    container.innerHTML = "";
 
     const filteredAssessments = assessments.filter((assessment) => {
       const dueDate = new Date(assessment.due_at);
@@ -101,7 +81,6 @@ function formatDate(dateString) {
   const minutes = date.getMinutes();
   const formattedDate = `${day}/${month}/${year} at ${hours}:${minutes}`;
 
-  // Calculate time remaining
   const timeDiff = date.getTime() - new Date().getTime();
   const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
   let colorClass = "";
